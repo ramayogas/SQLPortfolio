09765f533e102de7a34a6ae895ba7778013df01a
ramayogas <ramayogas.w@gmail.com>
2026-02-11 22:39:45 +0800

    Cleaning part 1, extracting clean name and title, but not finished on edge case

--- hospital-employee-name-standardization/queries/02_extract_name_titles.sql
+++ hospital-employee-name-standardization/queries/02_extract_name_titles.sql
@@ -2,6 +2,7 @@ ALTER TABLE employees ADD COLUMN clean_name TEXT;
 ALTER TABLE employees ADD COLUMN title TEXT;
 
 select full_name, clean_name, title from employees where profession_id = 1411
+SELECT * FROM employees
 
 UPDATE employees
 SET clean_name = TRIM(
@@ -10,6 +11,19 @@ SET clean_name = TRIM(
         --Before comma
         WHEN LENGTH(TRIM(SUBSTR(full_name,1,INSTR(full_name,',') - 1))) 
         >
+        --After comma
+             LENGTH(TRIM(SUBSTR(full_name, INSTR(full_name,',')+1)))
+          THEN SUBSTR(full_name,1,INSTR(full_name,',') - 1)
+        ELSE SUBSTR(full_name, INSTR(full_name,',')+1)
+    END
+)
+
+UPDATE employees
+SET title = TRIM(
+    CASE
+        --Before comma
+        WHEN LENGTH(TRIM(SUBSTR(full_name,1,INSTR(full_name,',') - 1))) 
+        <
         --After comma
              LENGTH(TRIM(SUBSTR(full_name, INSTR(full_name,',')+1)))
           THEN SUBSTR(full_name,1,INSTR(full_name,',') - 1)
